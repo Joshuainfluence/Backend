b433e16fe7f458a607cbea1b0ab292a876451d47
unknown <joshuajulius2030@gmail.com>
2024-10-21 17:42:17 +0100

    Loops done

---
+++ foreachLoop.php
@@ -0,0 +1,8 @@
+<?php
+// Loops through a block of code for each element in an array
+// It works only on arrays
+$users = array("John"=> 35, "peter"=>24, "Sophia"=>18, "Emma"=>30, "Influence"=>22);
+
+foreach($users as $key => $value){
+    echo "$key is $value \n";
+}
---
+++ index.py
@@ -0,0 +1,12 @@
+score = 70
+total = 100
+cutoff = total/2
+
+if(score < cutoff):
+    print("Sorry, you failed!")
+
+elif(score == cutoff):
+    print("YOu are on average")
+
+else:
+    print("Congratulations, you passed well!")
