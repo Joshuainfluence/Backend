979215541887764fd6d280108bc34a5f445b48b6
unknown <joshuajulius2030@gmail.com>
2025-01-11 15:10:34 +0100

    substraction

---
+++ functions/substraction.php
@@ -0,0 +1,11 @@
+<?php
+// Creating a function to perform simple substraction
+
+// This function accepts two parameter, and subtract one from another
+function substraction ($x, $y){
+    echo $x - $y;
+}
+
+// calling the function 
+// this will return the difference between the two values pass in place of the parameter.
+substraction(15, 7);
