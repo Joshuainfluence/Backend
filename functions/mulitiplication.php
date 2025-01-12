cb814c45002ac92895dc1e28527cc3e71fa48cd9
unknown <joshuajulius2030@gmail.com>
2025-01-12 19:14:04 +0100

    multiplication function

---
+++ functions/mulitiplication.php
@@ -0,0 +1,11 @@
+<?php
+// Creating a function to perform simple multiplication
+
+// This function accepts two parameter, and multiply them together to get
+function multiplication ($x, $y){
+    echo $x * $y;
+}
+
+// calling the function 
+// this will return the product of the two values pass in place of the parameter.
+multiplication(5, 7);
