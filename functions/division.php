b8c919092ced1f046dc3fcb8bede089fa9acc105
unknown <joshuajulius2030@gmail.com>
2025-01-13 17:55:03 +0100

    division function

---
+++ functions/division.php
@@ -0,0 +1,11 @@
+<?php
+// Creating a function to perform simple division
+
+// This function accepts two parameter, and divides one by the other
+function divisoin ($x, $y){
+    echo $x / $y;
+}
+
+// calling the function 
+// this will return the divided value of the first number by the seconde number.
+divisoin(12, 3);
