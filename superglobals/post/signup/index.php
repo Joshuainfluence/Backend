da36b1ec51634e11881da163f3409777b47d2f79
unknown <joshuajulius2030@gmail.com>
2025-04-09 15:34:20 +0100

    success and error alert on index page

--- superglobals/post/signup/index.php
+++ superglobals/post/signup/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start();
 ?>
 <!DOCTYPE html>
@@ -16,20 +16,37 @@ session_start();
         <div class="row">
             <div class="col-sm-12 col-md-12 col-lg-6 offset-lg-3 mt-5">
                 <h3>Create an account</h3>
-            <div class="card">
-                <div class="card-body">
-                    <form action="signup.php" method="post">
-                        <input type="text" name="username" id="username" class="form-control mb-2" placeholder="Enter usernam">
-                        <input type="email" name="email" id="email" class="form-control mb-2" placeholder="Enter email">
-                        <input type="password" name="password" id="password" class="form-control mb-2" placeholder="Enter password">
-                        <input type="submit" value="Signup" class="btn btn-lg btn-primary w-100">
-                    </form>
+                <!-- Error alert -->
+                <?php
+                if (isset($_SESSION['error'])) {
+                ?>
+                    <div class="alert alert-danger mt-2 mb-2"><?= $_SESSION['error'] ?></div>
+                <?php
+                }
+                ?>
+
+                <!-- Success alert -->
+                <?php
+                if (isset($_SESSION['success'])) {
+                ?>
+                    <div class="alert alert-success mt-2 mb-2"><?= $_SESSION['success'] ?></div>
+                <?php
+                }
+                ?>
+                <div class="card">
+                    <div class="card-body">
+                        <form action="signup.php" method="post">
+                            <input type="text" name="username" id="username" class="form-control mb-2" placeholder="Enter usernam">
+                            <input type="email" name="email" id="email" class="form-control mb-2" placeholder="Enter email">
+                            <input type="password" name="password" id="password" class="form-control mb-2" placeholder="Enter password">
+                            <input type="submit" value="Signup" class="btn btn-lg btn-primary w-100">
+                        </form>
+                    </div>
                 </div>
-            </div>
             </div>
         </div>
     </div>
 </body>
 
 </html>
-<?php unset($_SESSION['error'], $_SESSION['success'])?>
+<?php unset($_SESSION['error'], $_SESSION['success']) ?>
