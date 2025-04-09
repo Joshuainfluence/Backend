6ab878593cf78bb5a6226199fed7536b0059cad7
unknown <joshuajulius2030@gmail.com>
2025-04-09 15:35:34 +0100

    modified signup function with die after every validation

--- superglobals/post/signup/signup.php
+++ superglobals/post/signup/signup.php
@@ -10,21 +10,29 @@ function signup($username, $email, $password){
     if (empty($username) || empty($email) || empty($password)) {
         $_SESSION['error'] = "Fields cannot be empty";
         header("Location: index.php");
+        die();
     }
 
     if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
         $_SESSION['error'] = "Invalid email format";
         header("Location: index.php");
+        die();
+
     }
 
     if (strlen($password) < 8) {
         $_SESSION['error'] = "Password must be atleast 8 characters";
         header("Location: index.php");
+        die();
+
     }
 
     array_push($users, $username, $email, $password);
 
     $_SESSION['success'] = "Registration success!". print_r($users);
+    header("Location: index.php");
+    die();
+
 }
 
 echo signup($username, $email, $password);
