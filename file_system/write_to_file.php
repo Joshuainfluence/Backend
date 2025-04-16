880971b5a32fcd144f51591be7ebfb8c81a8d6d8
unknown <joshuajulius2030@gmail.com>
2025-04-16 11:36:05 +0100

    writing to file, a

--- file_system/write_to_file.php
+++ file_system/write_to_file.php
@@ -1,8 +1,14 @@
 <?php 
 
-$myfile = fopen("hello.txt", "w");
+// $myfile = fopen("hello.txt", "w");
+$myfile = fopen("hello.txt", "a");
+
+
 $name = readline("Enter username: ") . "\n";
+$password = readline("Enter password: ") . "\n";
 // fwrite() function is used to write to file
 fwrite($myfile, $name);
+fwrite($myfile, $password);
+
 // fclose function is used to close file, always at the end.
 fclose($myfile);
